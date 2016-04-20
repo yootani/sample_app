7d9870c8ea8f01d7eb9c728836872fa3130f84ff
ootani <y-ootani@sys-square.co.jp>
2016-04-20 16:31:37 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,9 @@ describe "User pages" do
 
   subject { page }
 
+
+
+
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -24,16 +27,26 @@ describe "User pages" do
        end
      end
 
+    # it "should create a user" do
+    #   expect { click_button submit }.to change(User, :count).by(1)
+     #end
+
      describe "with valid information" do
        before do
          fill_in "Name",         with: "Example User"
          fill_in "Email",        with: "user@example.com"
          fill_in "Password",     with: "foobar"
          fill_in "Confirmation", with: "foobar"
-       end
 
-       it "should create a user" do
-         expect { click_button submit }.to change(User, :count).by(1)
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) {User.find_by(email: 'user@example.com') }
+
+          it { should have_link('Sign out') }
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', test: 'Welcome') }
+        end
+
        end
      end
    end
